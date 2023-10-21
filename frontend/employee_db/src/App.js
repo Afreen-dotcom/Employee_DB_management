3462a8f9a4ee57e5715d4782b2ddd196b94bf56b
Afreen Bano <aaffy07@gmail.com>
2023-10-21 10:05:45 +0530

    Update App.js

--- frontend/employee_db/src/App.js
+++ frontend/employee_db/src/App.js
@@ -1,7 +1,5 @@
 import React, {Component} from 'react'
-//import {TableBody} from './MyComponent/Table2'
 import Form from './MyComponent/Form'
-//import MyTable from './MyComponent/MyTable';
 import {TableBody} from './MyComponent/Tablefinal'
 
 class App extends Component {
